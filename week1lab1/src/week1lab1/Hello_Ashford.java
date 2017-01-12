7f0ab0fff3558cdca12cdb7eec51bd862176677e
chazdiezal <cdandr4321kc@HQSWL-C005889.cable.comcast.com>
2017-01-12 02:28:11 -0500

    initial code

---
+++ week1lab1/src/week1lab1/Hello_Ashford.java
@@ -0,0 +1,22 @@
+package week1lab1;
+
+public class Hello_Ashford {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	
+    String name = "Charles Dandridge";
+    double Score1 = 97;
+    double Score2 = 75;
+    double Score3 = 87;
+    double Average = (Score1 + Score2 + Score3)/3;
+    //now you are ready to print
+    System.out.println(name);
+    System.out.println(Score1);
+    System.out.println(Score2);
+    System.out.println(Score3);
+    System.out.println(Average);	
+}
+
+}
