3ff93a656064277e83f08da1b88e70cf1cc6490b
chazdiezal <cdandr4321kc@HQSWL-C005889.cable.comcast.com>
2017-01-12 13:36:26 -0500

    updated score 3

--- week1lab1/src/week1lab1/Hello_Ashford.java
+++ week1lab1/src/week1lab1/Hello_Ashford.java
@@ -9,7 +9,7 @@ public class Hello_Ashford {
     String name = "Charles Dandridge";
     double Score1 = 97;
     double Score2 = 75;
-    double Score3 = 87;
+    double Score3 = 27;
     double Average = (Score1 + Score2 + Score3)/3;
     //now you are ready to print
     System.out.println(name);
