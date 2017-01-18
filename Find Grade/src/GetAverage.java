345f198595c8dbdb537c82130458d1cac1a745a2
chazdiezal <cdandr4321kc@HQSWL-C005889.cable.comcast.com>
2017-01-17 20:34:41 -0500

    fix average

--- Find Grade/src/GetAverage.java
+++ Find Grade/src/GetAverage.java
@@ -9,68 +9,67 @@ public class GetAverage {
 		// TODO Auto-generated method stub
 
 	
-	Scanner keyboard = new Scanner(System.in);
-	
-	
-	
-	int testScoreA;
-	int testScoreB;
-	int testScoreC;
-	int testScoreD;
-	int testScoreE;
-	String name1;
-	System.out.print("Enter your name: ");
-	name1 = keyboard.next();
-	
-	System.out.print("Enter your first test score: ");
-	testScoreA = keyboard.nextInt();
-	System.out.print("Enter your second test score: ");
-	testScoreB = keyboard.nextInt();
-	System.out.print("Enter your third test score: ");
-	testScoreC = keyboard.nextInt();
-	System.out.print("Enter your fourth test score: ");
-	testScoreD = keyboard.nextInt();
-	System.out.print("Enter your fifth test score: ");
-	testScoreE = keyboard.nextInt();
-	keyboard.nextLine();
-	double average = calcAverage(testScoreA,testScoreB, testScoreC, testScoreD, testScoreE);
-	System.out.println("The average is: + average");
-	determineGrade(average);
-	keyboard.close();
-	}
-	
-	public static double calcAverage(int testScoreA, int testScoreB, int testScoreC, int testScoreD, int testScoreE) 
-{
-	double average =(testScoreA + testScoreB + testScoreC + testScoreD + testScoreE)/5;
-	return average;
-	
-	
-}
-
-	public static double determineGrade(double average)
-{
-	if (average>90)
-	{
-		System.out.println("You got an A");
-	}
-	else if (average>=80)
-	{
-		System.out.println("You got a B");
-	}
-	else if (average>=70)
-	{
-		System.out.println("You got a C");
-	}
-	else if (average>=60)
-	{
-		System.out.println("You got a D");
-	}
-	else if (average<60)
-	{
-		System.out.println("You got an F");
-	}
-			return average;
+		Scanner keyboard = new Scanner(System.in);
 		
-}
-}
+		
+		
+		int testScoreA;
+		int testScoreB;
+		int testScoreC;
+		int testScoreD;
+		int testScoreE;
+		String name;
+		System.out.print("Enter your name: ");
+		name = keyboard.next();
+		
+		System.out.print("Enter your first test score: ");
+		testScoreA = keyboard.nextInt();
+		System.out.print("Enter your second test score: ");
+		testScoreB = keyboard.nextInt();
+		System.out.print("Enter your third test score: ");
+		testScoreC = keyboard.nextInt();
+		System.out.print("Enter your fourth test score: ");
+		testScoreD = keyboard.nextInt();
+		System.out.print("Enter your fifth test score: ");
+		testScoreE = keyboard.nextInt();
+		keyboard.nextLine();
+		double average = calcAverage(testScoreA,testScoreB, testScoreC, testScoreD, testScoreE);
+		System.out.println("The average is: " + average);
+		determineGrade(average);
+		keyboard.close();
+		}
+		
+		public static double calcAverage(int testScoreA, int testScoreB, int testScoreC, int testScoreD, int testScoreE) 
+	{
+		double average =(testScoreA + testScoreB + testScoreC + testScoreD + testScoreE)/5;
+		return average;
+		
+		
+	}
 
+		public static double determineGrade(double average)
+	{
+		if (average>90)
+		{
+			System.out.println("You got an A!");
+		}
+		else if (average>=80)
+		{
+			System.out.println("You got a B!");
+		}
+		else if (average>=70)
+		{
+			System.out.println("You got a C!");
+		}
+		else if (average>=60)
+		{
+			System.out.println("You got a D!");
+		}
+		else if (average<60)
+		{
+			System.out.println("You got an F!");
+		}
+				return average;
+			
+	}
+	}
