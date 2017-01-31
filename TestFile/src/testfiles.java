f0b22541cace5658ca6e08570a3a29b93f543903
chazdiezal <cdandr4321kc@HQSWL-C005889.cable.comcast.com>
2017-01-31 13:14:36 -0500

    initial cut

---
+++ TestFile/src/testfiles.java
@@ -0,0 +1,30 @@
+import java.io.*;  //needed to handle files
+public class testfiles {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+// The name of the file to open.
+	String week5file = "S:\\charles\\School\\Inf231\\week5\\week5file.txt";
+// This will reference one line at a time
+		String line = null;
+//below is the error handling if the file does not exist
+		try {
+			System.out.println("Charles");
+			FileReader filereader = new FileReader(week5file);
+			BufferedReader bufferedReader = new BufferedReader(fileReader);
+		while((line = bufferedReader.readLine()) != null)	{
+			System.out.println(line);
+		}
+		bufferedReader.close();  //closing file after processing
+		}
+		//error if the file does not exist
+		catch(FileNotFoundException ex) {
+			System.out.println("Unable to open file '")
+		}
+		catch(IOException ex) {
+			System.out.println( "Error reading file ");
+			
+		}
+	}
+
+}
