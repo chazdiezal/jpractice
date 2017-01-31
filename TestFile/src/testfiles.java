72be12fd30426891ca5ad7c4c43ad0001ebba250
chazdiezal <cdandr4321kc@HQSWL-C005889.cable.comcast.com>
2017-01-31 15:11:17 -0500

    final edition

--- TestFile/src/testfiles.java
+++ TestFile/src/testfiles.java
@@ -3,27 +3,33 @@ public class testfiles {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
 // The name of the file to open.
-	String week5file = "S:\\charles\\School\\Inf231\\week5\\week5file.txt";
+	String fileName = "S:\\charles\\School\\Inf231\\week5\\week5file.txt";
+	
 // This will reference one line at a time
 		String line = null;
+		
+		
 //below is the error handling if the file does not exist
 		try {
 			System.out.println("Charles");
-			FileReader filereader = new FileReader(week5file);
-			BufferedReader bufferedReader = new BufferedReader(fileReader);
+			FileReader filereader = new FileReader(fileName);
+			BufferedReader bufferedReader = new BufferedReader(new FileReader("S:\\charles\\School\\Inf231\\week5\\week5file.txt"));
 		while((line = bufferedReader.readLine()) != null)	{
 			System.out.println(line);
 		}
 		bufferedReader.close();  //closing file after processing
+		
 		}
+		
 		//error if the file does not exist
 		catch(FileNotFoundException ex) {
-			System.out.println("Unable to open file '")
+			System.out.println("Unable to open file '");
 		}
 		catch(IOException ex) {
 			System.out.println( "Error reading file ");
-			
+		
 		}
 	}
 
